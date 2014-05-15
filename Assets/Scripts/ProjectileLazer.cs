87a9e46279756eed4f34b9be162e1169e7754ed8
Cootzy <rcoetzee88@gmail.com>
2014-05-15 14:01:27 +1000

    Added boundary to battle scene. Destroys lazers when they leave the boundary

--- Assets/Scripts/ProjectileLazer.cs
+++ Assets/Scripts/ProjectileLazer.cs
@@ -6,12 +6,30 @@ public class ProjectileLazer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        rigidbody.velocity = transform.forward * speed;
-	
+        rigidbody.velocity = transform.forward * speed;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            //Destroys the lazer gameobject
+            Destroy(gameObject);
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        //If the lazer leaves the collider "Boundary" then the gameobject is destroyed
+        if (other.tag == "Boundary")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
