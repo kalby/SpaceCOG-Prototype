1df23d13ba2d0b4d946f3a7ebc0042caf1cff353
kalby <daniel.kalb@uqconnect.edu.au>
2014-05-21 07:07:42 +1000

    UIMission complete, UIFaction (RaceShip) ready for final assets and most of UILobby.

---
+++ Assets/Scripts/UI/LobbyControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LobbyControl : MonoBehaviour {
+
+    private UIPlaySound slideSound;
+
+	void Start () {
+        //Get the audio from the background, which is the slide sound
+        GameObject background = GameObject.FindWithTag("Background");
+        slideSound = background.GetComponent<UIPlaySound>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckKeys();
+	}
+
+    private void CheckKeys()
+    {
+        //if they hit escape load the previous scene
+        if (Input.GetKeyDown("escape"))
+        {
+            slideSound.Play();
+            while (slideSound.IsInvoking())
+            {
+                //do nothing
+            }
+            MissionSelect();
+        }
+    }
+
+    public void MissionSelect()
+    {
+        Application.LoadLevel("UIMission");
+    }
+}
