b1eca815298a6591fe3eea5ecce01343d093fe21
Roan <rcoetzee88@gmail.com>
2014-05-25 14:50:43 +1000

    Aeonur tracking improvement, Prefab changes, General code changes

---
+++ Assets/Scripts/Player/EnemyPlayerController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPlayerController : MonoBehaviour {
+    //This class is for everthing to do with the player except for flight
+
+    //Firing
+    public Transform[] turrets;
+    public GameObject lazer;
+    public float lazerRange;
+    public float fireRate;
+    public float lazerSpeed;
+    public float health;
+
+    //Explosion
+    public GameObject shipExplosion;
+
+    private float delayShot;
+    private int score;
+
+    private GameController gc;
+
+	// Use this for initialization
+	void Start () {
+        gc = GameObject.FindObjectOfType<GameController>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        ////Players shoots when spacebar is held down
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+            //Time delay to simulate a fire rate
+            //if (Time.time > delayShot)
+            //{
+            //    //Shoot Lazers from both turrets
+            //    foreach (Transform t in turrets)
+            //    {
+            //        GameObject lazerInstance;
+            //        lazerInstance = Instantiate(lazer, t.position, t.rotation) as GameObject;
+            //        lazerInstance.SendMessage("SetRange", lazerRange);
+            //        lazerInstance.rigidbody.AddForce(t.forward * lazerSpeed);
+            //        lazerInstance.GetComponent<ProjectileLazer>().player = transform;
+            //    }
+            //    delayShot = Time.time + fireRate;
+            //}
+        //}	
+	}
+
+    //This function is called when a gameobject enters the spaceships box collider.
+    void OnTriggerEnter(Collider other)
+    {
+        //if (other.tag == "Lazer")
+        //{
+        //    return;
+        //}
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            Destroy(gameObject);
+            //Remove this gameobject from the players list since it has been destroyed
+            gc.allPlayers.Remove(gameObject);
+
+            Instantiate(shipExplosion, transform.position, transform.rotation);
+        }
+    }
+
+    //This is mainly used to check that a message is being received once a lazer hits an enemy
+    void AddToScore(int points)
+    {        
+        score += points;
+        Debug.Log("Player score: " + score);
+    }
+
+    void Hit(int damage)
+    {
+        Debug.Log("Player Health: " + health);
+        if (health > 0)
+        {
+            health -= damage;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+                Instantiate(shipExplosion, transform.position, transform.rotation);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/EnemyPlayerFlight.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+public class EnemyPlayerFlight : MonoBehaviour
+{
+
+    //rotation
+    public float maxRollAngle;
+    public float rotationSpeed;
+
+    public float range;
+
+    //Particle System Engine
+    public GameObject[] engine;
+
+    //velocity variables
+    public float acceleration;
+    public float maxSpeed;
+    public float currentSpeed;
+    public float speedBoost;
+    //To do for later, I'll leave it in for now, the code using it is disabled -D
+    public float thrusterPower;
+
+    //private Variables
+    private float yaw = 0.0f;
+
+    private GameObject target;
+
+    private float delayTime;
+
+    private float horizontal;
+
+    void Start()
+    {
+        horizontal = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        //INPUTS
+        //set the inputs for wasd movement.
+        //should be in update not forcedUpdate() as "input flags aren't reset until update() is called."
+
+        //side-to-side.
+        
+        if (Time.time > delayTime)
+        {
+            horizontal = Random.Range(-1, 2);
+            delayTime = Time.time + 3f;
+        }
+        
+        //forwards and backwards.
+        float vertical = 1f;        
+        
+        //STEERING
+        //Adjust the facing as the player turns.
+        yaw = yaw + (horizontal * rotationSpeed);
+
+        //make the yaw angle loop around so it's not counting rotations.
+        //prevents possible overflow.
+        if (yaw > 360 || yaw < -360)
+        {
+            yaw = 0;
+        }
+
+        //VELOCITY CONTROL
+        currentSpeed = currentSpeed + (vertical * acceleration);
+        if (currentSpeed < 0f)
+        {
+            currentSpeed = 0f;
+            
+        }
+        else if (currentSpeed > maxSpeed)
+        {
+            currentSpeed = maxSpeed;
+        }
+
+        //Particle System control
+        if (currentSpeed <= 0f)
+        {
+            foreach (GameObject p in engine)
+            {
+                p.particleSystem.enableEmission = false;
+            }
+            
+        }
+        else
+        {
+            foreach (GameObject p in engine)
+            {
+                p.particleSystem.enableEmission = true;
+            }
+        }
+
+        //AXES
+        //cannot use raw transform.forward due to the rotation creating non-zero y component.
+        float tx = transform.forward.x;
+        float tz = transform.forward.z;
+        
+        //create the ship facing vector in the XZ plane.
+        Vector3 facingXZ = new Vector3(tx, 0.0f, tz);
+        //create the horizontal vector in the XZ plane.
+        //Vector3 orthogonalXZ = Vector3.Cross(facingXZ, Vector3.up);
+
+        //MOMENTUM
+        //give it forward momentum in the XZ plane.
+        transform.Translate(facingXZ * Time.deltaTime * currentSpeed, Space.World);
+
+        //give it side-to-side momentum, emulates another engine firing to the side. Use to implement thrusters later.
+        //transform.Translate((horizontal) * orthogonalXZ * Time.deltaTime * thrusterPower, Space.World);
+
+        //ROTATION
+        Quaternion rotation = Quaternion.Euler(0.0f, yaw, -maxRollAngle * horizontal * Mathf.Sign(currentSpeed));
+        //Use slerp to sexy (spherical interpolate) up that rotation!
+        Quaternion slerpedRotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 10);
+        transform.rotation = slerpedRotation;        
+    }
+}
